5872b7a0bb5541633ae110c232f7233653b32d19
Oleksandr Sudarchuk <oleksandrsudarchuk@gmail.com>
2025-05-27 19:12:33 +0200

    Deleted Storyboard, upgrate func scene in SceneDelegate, create view and made it like main view in view controller

---
+++ Omnos/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Omnos
+//
+//  Created by Oleksandr Sudarchuk on 27/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let welcomeView = WelcomeView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func loadView() {
+        view = welcomeView
+    }
+
+}
+
---
+++ Omnos/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  Omnos
+//
+//  Created by Oleksandr Sudarchuk on 27/05/2025.
+//
+
+import UIKit
+import SnapKit
+
+
+class WelcomeView: UIView {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
